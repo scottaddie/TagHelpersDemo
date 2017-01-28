2a537af9d6ddfa95cd2d7d697edcdb82d7dbf56c
Scott Addie <tobias.addie@gmail.com>
2017-01-27 22:50:57 -0600

    create constants class to store tag helper element names

---
+++ TagHelpersDemo/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TagHelpersDemo
+{
+    internal class Constants
+    {
+        public const string CARD_TAG_HELPER_ELEMENT_NAME = "card";
+        public const string HAND_TAG_HELPER_ELEMENT_NAME = "hand";
+    }
+}
--- TagHelpersDemo/TagHelpers/HandTagHelper.cs
+++ TagHelpersDemo/TagHelpers/HandTagHelper.cs
@@ -7,19 +7,16 @@ namespace TagHelpersDemo.TagHelpers
         public string Player { get; set; }
     }
 
-    //[HtmlTargetElement("hand", Attributes = nameof(NumCards) + "," + nameof(Player), TagStructure = TagStructure.NormalOrSelfClosing)]
-    [HtmlTargetElement("hand", Attributes = nameof(Player), TagStructure = TagStructure.NormalOrSelfClosing)]
-    [RestrictChildren("card")]
+    [HtmlTargetElement(Constants.HAND_TAG_HELPER_ELEMENT_NAME, Attributes = nameof(Player), TagStructure = TagStructure.NormalOrSelfClosing)]
+    [RestrictChildren(Constants.CARD_TAG_HELPER_ELEMENT_NAME)]
     public class HandTagHelper : TagHelper
     {
-        //public int NumCards { get; set; }
-
         public string Player { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             // Assign the lowercase version of the player's name to the appropriate context property
-            var handContext = new HandContext()
+            var handContext = new HandContext
             {
                 Player = Player.Trim().ToLower()
             };

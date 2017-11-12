b229973e64c898d1bd1d7b0413f405d3a8d7fe73
Scott Addie <scott.addie@microsoft.com>
2017-11-12 11:06:07 +0200

    More code reformatting

--- TagHelpersDemo/Startup.cs
+++ TagHelpersDemo/Startup.cs
@@ -9,7 +9,8 @@ namespace TagHelpersDemo
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration) => Configuration = configuration;
+        public Startup(IConfiguration configuration) => 
+            Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
